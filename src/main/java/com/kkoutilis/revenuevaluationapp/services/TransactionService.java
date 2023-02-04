f00d1b238c6a9276f100203e23d728ec80566f46
koutsilis1999 <kkoutsilis@gmai.com>
2023-02-04 17:38:22 +0000

    calcReport method

--- src/main/java/com/kkoutilis/revenuevaluationapp/services/TransactionService.java
+++ src/main/java/com/kkoutilis/revenuevaluationapp/services/TransactionService.java
@@ -1,11 +1,13 @@
 package com.kkoutilis.revenuevaluationapp.services;
 
+import com.kkoutilis.revenuevaluationapp.DTOs.Report;
 import com.kkoutilis.revenuevaluationapp.models.Transaction;
 import com.kkoutilis.revenuevaluationapp.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransactionService {
@@ -23,5 +25,18 @@ public class TransactionService {
     public Transaction create(Transaction transaction) {
         this.repository.save(transaction);
         return transaction;
+    }
+
+    public boolean delete(UUID id) {
+        Transaction transaction = this.repository.findById(id).orElseThrow();
+        this.repository.delete(transaction);
+        return true;
+    }
+
+    public Report calcReport() {
+        float income = this.repository.getIncomeSum();
+        float expenses = this.repository.getExpensesSum();
+        float revenue = income - expenses;
+        return new Report(income, expenses, revenue);
     }
 }
