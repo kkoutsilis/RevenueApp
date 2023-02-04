78ffd0340f51b06d7fb2aa4c6b361990b0b2562c
koutsilis1999 <kkoutsilis@gmai.com>
2023-02-04 17:38:01 +0000

    transactions repo queries to get sum of income and expenses

--- src/main/java/com/kkoutilis/revenuevaluationapp/repositories/TransactionRepository.java
+++ src/main/java/com/kkoutilis/revenuevaluationapp/repositories/TransactionRepository.java
@@ -2,9 +2,17 @@ package com.kkoutilis.revenuevaluationapp.repositories;
 
 import com.kkoutilis.revenuevaluationapp.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    @Query(value = "SELECT COALESCE(SUM(amount),0) FROM transactions WHERE type = 'income'",nativeQuery = true)
+    float getIncomeSum();
+
+    @Query(value = "SELECT COALESCE(SUM(amount),0) FROM transactions WHERE type = 'expense'",nativeQuery = true)
+    float getExpensesSum();
+
 }
